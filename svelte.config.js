f9d76b8705ecf6e441aeff2ef9ac9cb579f72727
Abazarfard <91885900+Abazarfard@users.noreply.github.com>
2025-01-23 18:49:02 +0330

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -16,3 +16,16 @@ export default {
     }
   }
 };
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter({
+			fallback: '404.html'
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
+	}
+};
+
+export default config;
