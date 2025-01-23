92cdd1bbd8662afc0ff918eec65d2600e00727c1
Abazarfard <91885900+Abazarfard@users.noreply.github.com>
2025-01-23 19:00:06 +0330

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,7 @@ const config = {
       strict: true
     }),
     paths: {
-      base: process.env.NODE_ENV === 'production' ? '/https://github.com/Abazarfard/CP.git' : ''
+      base: process.env.NODE_ENV === 'production' ? '/https://github.com/Abazarfard/CP.git/.svelte-kit' : ''
     }
 };
 
